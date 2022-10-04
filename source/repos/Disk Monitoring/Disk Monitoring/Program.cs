36c8327f63c27b499647ecfe41f832249f72f37b
Lestrn <ilestrn@gmail.com>
2022-10-04 12:45:49 +0300

    Trying to fix multiple msgs

---
+++ source/repos/Disk Monitoring/Disk Monitoring/Program.cs
@@ -0,0 +1,110 @@
+﻿
+
+namespace Disk_Monitoring
+{
+    class DiskMonitoring
+    {
+        public string tempPath { get; private set; }
+        public bool PathIsChanged { get; set; }
+        public void GetAvailableDisks()
+        {
+            string[] availableDisks = Directory.GetLogicalDrives();
+            Console.WriteLine("Available disks");
+            foreach (var disk in availableDisks)
+            {
+                Console.WriteLine(disk);
+            }
+        }
+        public  string GetPathFromUser()
+        {
+            string diskPath;
+            do
+            {
+                diskPath = Console.ReadLine();
+                if (Path.IsPathFullyQualified(diskPath))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("You path / disk was unknown");
+                }
+
+            } while (true);
+            return diskPath;
+        }
+        public  void WatcherChanged(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"Directory changed with path: {e.FullPath}\nType: {e.ChangeType}\nName: {e.Name}");
+            if (e.ChangeType == WatcherChangeTypes.Changed)
+            {
+                PathIsChanged = true;
+                tempPath = e.FullPath;
+                new Thread(this.MonitoringDirectories).Start(this.tempPath);
+            }
+        }
+        public  void WatcherRenamed(object sender, RenamedEventArgs e)
+        {
+            Console.WriteLine($"Directory was renamed old path: {e.OldFullPath}\n" +
+                $"New path  {e.FullPath}\n" +
+                $"Old Name: {e.OldName}\n" +
+                $"New Name: {e.Name}");
+        }
+        public  void DiskMemoryLeft(string diskPath)
+        {
+            long diskSizeLeftByte = new DriveInfo(diskPath).AvailableFreeSpace;
+            int diskSizeLeftMb = (int)(diskSizeLeftByte / 1048576);
+            Console.WriteLine($"Total disk space left: {diskSizeLeftMb}mb");
+        }
+        public  void GetGeneralInfo(string diskPath)
+        {
+            long diskSizeByte = new DriveInfo(diskPath).TotalSize;
+            long diskSizeLeftByte = new DriveInfo(diskPath).AvailableFreeSpace;
+            int diskSizeMb = (int)(diskSizeByte / 1048576);
+            Console.WriteLine($"Ok i will be monitoring {Path.GetPathRoot(diskPath)}");
+            Console.WriteLine($"Total disk space: {diskSizeMb}mb");
+        }
+        public void MonitoringDirectories(object pathObject)
+        {
+            string path = pathObject as string;
+            FileSystemWatcher watcher = new FileSystemWatcher() { Path = path };
+            watcher.EnableRaisingEvents = true;
+            watcher.Renamed += new RenamedEventHandler(WatcherRenamed);
+            watcher.Changed += new FileSystemEventHandler(WatcherChanged);
+            var change = watcher.WaitForChanged(WatcherChangeTypes.All);
+            Console.WriteLine($"Type of change: {change.ChangeType}\nName:{change.Name}");
+            watcher.Deleted += new FileSystemEventHandler(WatcherChanged);
+        }
+    }
+    class Program
+    {
+        private static async Task Main()
+        {
+            DiskMonitoring monitorer = new DiskMonitoring();
+            monitorer.GetAvailableDisks();
+            
+            Console.WriteLine("Welcome! which disk should I monitor?");
+            string diskPath = monitorer.GetPathFromUser();
+            while (true)
+            {
+                monitorer.GetGeneralInfo(diskPath);
+                monitorer.DiskMemoryLeft(diskPath);
+                new Thread(monitorer.MonitoringDirectories).Start(diskPath);
+                //new Thread(() =>
+                //{
+                //    if (monitorer.PathIsChanged)
+                //    {
+                //        new Thread(monitorer.MonitoringDirectories).Start(monitorer.tempPath);
+                //        monitorer.PathIsChanged = false;
+                //    }
+                //}).Start();
+                string userStop = Console.ReadLine();
+                if(userStop == "stop")
+                {
+                    break;
+                }
+                
+            }
+        }
+    }
+}
